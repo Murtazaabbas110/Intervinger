01b11093cd65f68267ad1fb34c4a3469c460bc34
Murtazaabbas110 <aghamurtaza1101@gmail.com>
2025-11-09 23:51:07 +0500

    integrate clerk authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
+import { connectDb } from "./lib/db.js";
 const app = express();
 const __dirname = path.resolve();
 
@@ -10,13 +11,23 @@ app.get("/healthy", (req, res) => {
 app.get("/books", (req, res) => {
   res.status(200).json({ msg: "success from Books API" });
 });
-app.listen(ENV.PORT, () => {
-  console.log("Success running: ", ENV.PORT);
-});
+
+const startServer = async () => {
+  try {
+    await connectDb();
+    app.listen(ENV.PORT, () => {
+      console.log("Success running: ", ENV.PORT);
+    });
+  } catch (error) {
+    console.error("Some Error: ", error);
+  }
+};
+
+startServer();
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/{*any}",(req,res)=>{
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-  })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
