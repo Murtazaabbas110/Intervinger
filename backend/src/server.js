ea530b4ef12e63766561839bed53d54a593abdc8
Murtazaabbas110 <aghamurtaza1101@gmail.com>
2025-11-09 22:33:04 +0500

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,22 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import path from "path";
 const app = express();
-app.get("/", (req, res) => {
+const __dirname = path.resolve();
+
+app.get("/healthy", (req, res) => {
   res.status(200).json({ msg: "success from API" });
+});
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "success from Books API" });
 });
 app.listen(ENV.PORT, () => {
   console.log("Success running: ", ENV.PORT);
 });
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}",(req,res)=>{
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+  })
+}
