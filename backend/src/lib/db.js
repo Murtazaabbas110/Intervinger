6a68f6dadaaf9f6eb12efb02708b98e94b9e90a1
Murtazaabbas110 <aghamurtaza1101@gmail.com>
2025-11-14 17:16:14 +0500

    Validation Check added for DB_URL

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import { ENV } from "./env.js";
 
 export const connectDb = async () => {
   try {
+    if (!ENV.DB_URL) {
+      throw new Error("DB URL not defined in Enviorment Variables");
+    }
     const conn = await mongoose.connect(ENV.DB_URL);
     console.log("Connected To DB: ", conn.connection.host);
   } catch (error) {
