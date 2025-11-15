eb029a20612de63522d2d242d27e4db6897d1e20
Murtazaabbas110 <aghamurtaza1101@gmail.com>
2025-11-16 01:02:11 +0500

    updating stream.js

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -5,7 +5,7 @@ const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
 
 if (!apiKey || !apiSecret) {
-  console.log("Stream API Key or Stream Secret Key Is Missing");
+  console.error("Stream API Key or Stream Secret Key Is Missing");
 }
 
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
