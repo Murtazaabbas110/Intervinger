191a27086060d5c40596aae6ccdfcb22ef95d011
Murtazaabbas110 <aghamurtaza1101@gmail.com>
2025-11-15 21:51:23 +0500

    Stream connection

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "intervinger" });
 
@@ -22,7 +23,11 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    //more code to be added
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   }
 );
 
@@ -35,7 +40,7 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
 
-    //more code to be added
+    await deleteStreamUser(id.toString());
   }
 );
 
